94fcbd6512ddddd36fdf9b1ea200620e02222380
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-08-07 21:23:33 -0300

    Criado campos de pesquisa

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Text, TextInput } from 'react-native';
 
 import PageHeader from '../components/PageHeader';
 
@@ -9,7 +9,39 @@ import TeacherItem from '../components/TeacherItem';
 function TeacherList() {
   return(
     <View style={styles.container}>
-      <PageHeader title='Proffys disponíveis'/>
+      <PageHeader title='Proffys disponíveis'>
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matéria</Text>
+          <TextInput
+            placeholderTextColor='#C1BCCC'
+            style={styles.input}
+            placeholder='Qual a matéria?'
+          />
+
+          <View style={styles.inputGroup}>
+
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da semana</Text> 
+              <TextInput
+                placeholderTextColor='#C1BCCC'
+                style={styles.input}
+                placeholder="Qual o dia?"
+              />
+            </View>
+
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text> 
+              <TextInput
+                placeholderTextColor='#C1BCCC'
+                style={styles.input}
+                placeholder="Qual horário?"
+              />
+            </View>
+
+          </View>
+
+        </View>
+      </PageHeader>
 
       <ScrollView
         style={styles.teacherList}
